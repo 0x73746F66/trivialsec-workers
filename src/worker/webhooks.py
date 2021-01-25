5420208069aab66f86d2df384f4deec37813fdb1
Christopher Langton <chris.langton@cmd.com.au>
2021-01-25 23:43:57 +1100

    added outgoing webhook

---
+++ src/worker/webhooks.py
@@ -0,0 +1,36 @@
+from os import path
+from urllib.parse import urlsplit
+from datetime import datetime
+import hmac
+import hashlib
+import json
+import requests
+from retry.api import retry
+from trivialsec.helpers.log_manager import logger
+from trivialsec.models.webhook import Webhooks
+
+
+@retry((ConnectionError), tries=5)
+def send_webhook(account_id: int, event_name: str, data: dict, http_method: str = 'POST'):
+    json_data = json.dumps(data, sort_keys=True, default=str)
+    for webhook in Webhooks().find_by([('account_id', account_id), ('active', 1)]):
+        target = urlsplit(webhook.target)
+        target_url = f'https://{target.netloc}{path.join(target.path, event_name)}'
+        logger.debug(f"Sending webhook {target_url}")
+        try:
+            now = datetime.utcnow()
+            request = requests.Request(http_method, target_url, json=json_data, headers={
+                'Content-Type': 'application/json',
+                'X-Digest': 'HMAC-SHA3-512',
+                'X-Date': now.isoformat()
+            })
+            prepped = request.prepare()
+            signature = f'{http_method}\n{prepped.body}\n{now.isoformat()}'
+            ciphertext = hmac.new(webhook.webhook_secret, signature, digestmod=hashlib.sha3_512)
+            prepped.headers['X-Signature'] = ciphertext.hexdigest()
+            with requests.Session() as session:
+                session.send(prepped)
+
+        except Exception as err:
+            logger.exception(err)
+            continue
