2496df6cb2741507ea63a2063dbc1ca17c44064b
Christopher Langton <chris.langton@cmd.com.au>
2021-01-26 23:21:29 +1100

    log cron attempts

--- deploy/user-data/worker.sh
+++ deploy/user-data/worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-runuser -l ec2-user -c 'circusd --daemon -- /srv/app/circus.ini'
+echo '* * * * * ec2-user /usr/bin/pgrep circusd || circusd --daemon -- /srv/app/circus.ini >/tmp/application.log 2>&1' > /etc/cron.d/circusd-up
